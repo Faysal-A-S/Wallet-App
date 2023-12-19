f0c9c93c77b9f2f807ac83919e099418279ea7e7
Faysal Ahmed <hl.faysal1@gmail.com>
2023-12-19 23:32:18 +0600

    test

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ import transactionRouter from "./routes/transactionRoutes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.options("*", cors());
+app.use(cors());
 app.use("/api/users", UserRouter);
 app.use("/api/transactions", transactionRouter);
 app.use("/api/requests", requestRouter);
